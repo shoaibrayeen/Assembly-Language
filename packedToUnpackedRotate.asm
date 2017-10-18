ed2fb975da1ebc8887be48269b465d4e5e21dfab
MOHD SHOAIB RAYEEN <31645414+shoaibrayeen@users.noreply.github.com>
2017-10-19 01:14:51 +0530

    Packed to Unpacked using Rotate Instruction

---
+++ packedToUnpackedRotate.asm
@@ -0,0 +1,30 @@
+DATA SEGMENT                
+    PACKED      DB  78h,56h,34h,12h,13h,54h,28h,94h
+    UNPACKED    DB  16 DUP(?)
+    
+DATA ENDS
+
+CODE SEGMENT
+    ASSUME  DS:DATA 
+    
+    START :
+            MOV AX, DATA
+            MOV DS, AX
+            
+            MOV CX,8
+            MOV SI,0 
+            MOV DI,0 
+            
+    NEXT:
+            XOR AX,AX 
+		    MOV AL,PACKED[SI]
+		    INC SI
+		    ROL AX,4
+		    ROR AL,4
+		    MOV UNPACKED[DI], AH
+		    INC DI
+		    MOV UNPACKED[DI],AL
+		    INC DI
+            LOOP NEXT
+CODE ENDS  
+    END START
